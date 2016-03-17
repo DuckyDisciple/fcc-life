4ba95df847b594f9bbf914a103d00786993cc0e9
Drew Sartorius <duckydesign@gmail.com>
2016-03-17 09:34:28 -0400

    Controls added with reset button working

--- JS_Babel_React.js
+++ JS_Babel_React.js
@@ -2,7 +2,7 @@ var Gameboard = React.createClass({
   render: function(){
     var cols = [];
     for(var i=0; i<30; i++){
-      cols.push(<Col />);
+        cols.push(<Col colNum={i} curBoard={this.props.board} />);
     }
     return (
       <div className="gameboard">
@@ -16,7 +16,11 @@ var Col = React.createClass({
   render: function(){
     var rows = [];
     for(var i=0; i<30; i++){
-      rows.push(<div className="square" />);
+      if(this.props.curBoard[this.props.colNum][i]===0){
+        rows.push(<div className="square" />);
+      }else{
+        rows.push(<div className="square alive" />);
+      }
     }
     return (
       <div className="column">
@@ -26,4 +30,53 @@ var Col = React.createClass({
   }
 });
 
-React.render(<Gameboard />, document.getElementById("container"));
+var Controls = React.createClass({
+  play: function(){
+    this.props.playClicked();
+  },
+  reset: function(){
+    this.props.resetClicked();
+  },
+  render: function(){
+    return (
+      <div className="controls">
+        <button className="play" onClick={this.play}>Play</button>
+        <button className="reset" onClick={this.reset}>Reset</button>
+        <button className="clear">Clear</button>
+      </div>
+    );
+  }
+});
+
+var Container = React.createClass({
+  getInitialState: function(){
+    return {mounted: false, board:[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]};
+  },
+  componentDidMount: function(){
+    this.setState({mounted: true});
+  },
+  playClick: function(){
+    
+  },
+  resetClick: function(){
+    var newBoard = [];
+    for(var i=0; i<30; i++){
+      var col = [];
+      for(var j=0; j<30; j++){
+        col.push(Math.round(Math.random()));
+      }
+      newBoard.push(col);
+    }
+    this.setState({board: newBoard});
+  },
+  render: function(){
+    return (
+      <div className="conatiner">
+        <Gameboard board={this.state.board} />
+        <Controls playClicked={this.playClick} resetClicked={this.resetClick} />
+      </div>
+    );
+  }
+});
+
+React.render(<Container />, document.getElementById("container"));
